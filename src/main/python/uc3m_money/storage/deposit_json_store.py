78ad16115e0b592bf12195b2a9170dc4b3fbf348
AlexPMontes <100499978@alumnos.uc3m.es>
2025-04-24 02:48:12 +0200

    refactor: implemented singleton on DepositJsonStore

--- src/main/python/uc3m_money/storage/deposit_json_store.py
+++ src/main/python/uc3m_money/storage/deposit_json_store.py
@@ -2,9 +2,17 @@ from .json_store import JsonStore
 from ..account_management_config import DEPOSITS_STORE_FILE
 
 
-class DepositJsonStore(JsonStore):
-    _file_name = DEPOSITS_STORE_FILE
+class DepositJsonStore:
+    class __DepositJsonStore(JsonStore):
+        _file_name = DEPOSITS_STORE_FILE
 
-    def add_item(self, item):
-        self._data_list.append(item.to_json())
-        self.save_list_to_file()
+        def add_item(self, item):
+            self._data_list.append(item.to_json())
+            self.save_list_to_file()
+
+    __instance = None
+
+    def __new__(cls):
+        if not DepositJsonStore.__instance:
+            DepositJsonStore.__instance = DepositJsonStore.__DepositJsonStore()
+        return DepositJsonStore.__instance
