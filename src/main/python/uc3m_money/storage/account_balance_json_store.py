f1ee80815b073b151cbe28b0c0fa9c75245aab06
AlexPMontes <100499978@alumnos.uc3m.es>
2025-04-24 02:49:56 +0200

    refactor: implemented singleton on AccountBalanceJsonStore

--- src/main/python/uc3m_money/storage/account_balance_json_store.py
+++ src/main/python/uc3m_money/storage/account_balance_json_store.py
@@ -2,5 +2,13 @@ from .json_store import JsonStore
 from ..account_management_config import BALANCES_STORE_FILE
 
 
-class AccountBalanceJsonStore(JsonStore):
-    _file_name = BALANCES_STORE_FILE
+class AccountBalanceJsonStore:
+    class __AccountBalanceJsonStore(JsonStore):
+        _file_name = BALANCES_STORE_FILE
+
+    __instance = None
+
+    def __new__(cls):
+        if not AccountBalanceJsonStore.__instance:
+            AccountBalanceJsonStore.__instance = AccountBalanceJsonStore.__AccountBalanceJsonStore()
+        return AccountBalanceJsonStore.__instance
