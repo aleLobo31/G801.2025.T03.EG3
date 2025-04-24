f0d8edbbe3b1b3de242339452c5170968e17058c
AlexPMontes <100499978@alumnos.uc3m.es>
2025-04-24 16:28:41 +0200

    refactor: implemented singleton on TransactionJsonStore

--- src/main/python/uc3m_money/storage/transaction_json_store.py
+++ src/main/python/uc3m_money/storage/transaction_json_store.py
@@ -1,13 +1,18 @@
 from .json_store import JsonStore
 from ..account_management_config import TRANSACTIONS_STORE_FILE
 
-class TransactionJsonStore(JsonStore):
+class TransactionJsonStore:
+    class __TransactionJsonStore(JsonStore):
+        def __init__(self):
+            self._file_name = TRANSACTIONS_STORE_FILE
 
-    def __init__(self):
-        self._file_name = TRANSACTIONS_STORE_FILE
+        def load_list_from_file(self, fnf_error=False):
+            super().load_list_from_file(fnf_error)
+            return self._data_list
 
-    def load_list_from_file(self, fnf_error=False):
-        super().load_list_from_file(fnf_error)
-        return self._data_list
+    __instance = None
 
-
+    def __new__(cls):
+        if not TransactionJsonStore.__instance:
+            TransactionJsonStore.__instance = TransactionJsonStore.__TransactionJsonStore()
+        return TransactionJsonStore.__instance
