d9b3eb534bc05ff7e54a5b2e19ecd1dfeee16c05
AlexPMontes <100499978@alumnos.uc3m.es>
2025-04-24 16:48:10 +0200

    refactor: implemented singleton on InputDepositJsonStore

--- src/main/python/uc3m_money/storage/input_deposit_json_store.py
+++ src/main/python/uc3m_money/storage/input_deposit_json_store.py
@@ -1,10 +1,19 @@
 from .json_store import JsonStore
 
-class InputDepositJsonStore(JsonStore):
+class InputDepositJsonStore:
+    class __InputDepositJsonStore(JsonStore):
+        def __init__(self, input_file):
+            self._file_name = input_file
 
-    def __init__(self, input_file):
-        self._file_name = input_file
+        def load_list_from_file(self, fnf_error=False):
+            super().load_list_from_file(fnf_error)
+            return self._data_list
 
-    def load_list_from_file(self, fnf_error=False):
-        super().load_list_from_file(fnf_error)
-        return self._data_list
+    __instance = None
+
+    def __new__(cls, input_name):
+        if not InputDepositJsonStore.__instance:
+            InputDepositJsonStore.__instance = InputDepositJsonStore.__InputDepositJsonStore(input_name)
+        else:
+            InputDepositJsonStore.__instance._file_name = input_name
+        return InputDepositJsonStore.__instance
